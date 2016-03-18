114d733ce826c24749e34f30ddedcc7e3eda33bb
Davide Tampellini <tampe125@gmail.com>
2016-03-18 23:05:18 +0100

    Converted the script into an object

--- hashtag-scraper.py
+++ hashtag-scraper.py
@@ -1,46 +1,101 @@
 import json
 import twitter
+from datetime import date
+from os import path as os_path
+from lib.exceptions import exceptions
 
-with open('settings.json') as f_settings:
-    settings = json.load(f_settings)
 
-connection = twitter.Api(consumer_key=settings['app_key'],
-                         consumer_secret=settings['app_secret'],
-                         access_token_key=settings['token'],
-                         access_token_secret=settings['token_secret'])
+class HashtagsScraper:
+    def __init__(self):
 
-# Let's check if we really have some valid credentials
-connection.VerifyCredentials()
+        self.settings = None
+        self.version = '1.0.0'
 
-tweets = connection.GetStreamSample()
-words = set([])
+    def checkenv(self):
+        if not os_path.exists(os_path.realpath("settings.json")):
+            raise exceptions.InvalidSettings("Please rename the file settings-dist.json to settings.json and fill the required info")
 
-for tweet in tweets:
-    # Do we have to dump the collected dumps?
-    if len(words) >= 100:
-        with open('wordlist_hashtag', 'a') as f_wordlist:
-            f_wordlist.write('\n'.join(words) + '\n')
+        with open(os_path.realpath("settings.json")) as f_settings:
+            settings = json.load(f_settings)
+
+        required_keys = ['app_key', 'app_secret', 'token', 'token_secret']
+
+        for required in required_keys:
+            try:
+                value = settings[required]
+
+                if value == '':
+                    raise exceptions.InvalidSettings("Please fill the required info '" + required + "' before continuing")
+
+            except KeyError:
+                raise exceptions.InvalidSettings("Please fill the required info '" + required + "' before continuing")
+
+        self.settings = settings
+
+    def banner(self):
+        print("Hashtag Scraper " + self.version + " - A better way of scraping")
+        print("Copyright (C) " + str(date.today().year) + " FabbricaBinaria - Davide Tampellini")
+        print("===============================================================================")
+        print("Dump Scraper is Free Software, distributed under the terms of the GNU General")
+        print("Public License version 3 or, at your option, any later version.")
+        print("This program comes with ABSOLUTELY NO WARRANTY as per sections 15 & 16 of the")
+        print("license. See http://www.gnu.org/licenses/gpl-3.0.html for details.")
+        print("===============================================================================")
+
+    def run(self):
+        self.banner()
+
+        # Perform some sanity checks
+        try:
+            self.checkenv()
+        except exceptions.InvalidSettings as error:
+            print error
+            return
+
+        connection = twitter.Api(consumer_key=self.settings['app_key'],
+                                 consumer_secret=self.settings['app_secret'],
+                                 access_token_key=self.settings['token'],
+                                 access_token_secret=self.settings['token_secret'])
+
+        # Let's check if we really have some valid credentials
+        connection.VerifyCredentials()
+
+        tweets = connection.GetStreamSample()
         words = set([])
 
-    # Skip non english tweets
-    try:
-        language = tweet['lang']
-    except KeyError:
-        continue
+        for tweet in tweets:
+            # Do we have to dump the collected dumps?
+            if len(words) >= 100:
+                with open('wordlist_hashtag.txt', 'a') as f_wordlist:
+                    f_wordlist.write('\n'.join(words) + '\n')
+                words = set([])
 
-    if language != 'en':
-        continue
+            # Skip non english tweets
+            try:
+                language = tweet['lang']
+            except KeyError:
+                continue
 
-    # If we don't have an hashtag, let's skip the tweet
-    try:
-        hashtags = tweet['entities']['hashtags']
-    except KeyError:
-        continue
+            if language != 'en':
+                continue
 
-    for hashtag in hashtags:
-        text = hashtag['text']
-        # We're interested in words of 10 chars at least
-        if len(text) < 10:
-            continue
+            # If we don't have an hashtag, let's skip the tweet
+            try:
+                hashtags = tweet['entities']['hashtags']
+            except KeyError:
+                continue
 
-        words.add(text)
+            for hashtag in hashtags:
+                text = hashtag['text']
+                # We're interested in words of 10 chars at least
+                if len(text) < 10:
+                    continue
+
+                words.add(text)
+
+
+try:
+    scraper = HashtagsScraper()
+    scraper.run()
+except KeyboardInterrupt:
+    print("Operation aborted")
